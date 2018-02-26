28aec409689c13e1125e9a4170161b7eca1bf91f
Christiaan Hartman <c.hartman.cit@gmail.com>
2018-02-26 23:06:55 +0100

    Led blinky based on svd2rust

---
+++ stm32f103-svd/src/main.rs
@@ -0,0 +1,45 @@
+#![no_std]
+#![feature(asm)]
+
+extern crate stm32f103xx;
+
+use stm32f103xx::GPIOC;
+
+fn main()  {
+    let per = stm32f103xx::Peripherals::take().unwrap();
+
+    // Enable port c in the register clock controll
+    per.RCC.apb2enr.write(|w| w.iopcen().enabled());
+
+    let gpioc = per.GPIOC;
+
+    // Change PIN13 mode to output
+    gpioc.crh.write(|w| w.mode13().output());
+    
+    loop {
+        // Blink 10 times quickly (250ms on 250ms off)
+        for _ in 0..10 {
+            blink(&gpioc);
+        }
+        delay_ms(1000);
+    }
+
+}
+
+fn blink(gpioc: &GPIOC) {
+    // Turn led on
+    gpioc.bsrr.write(|w| w.bs13().set());
+    delay_ms(250);
+    // Turn led off
+    gpioc.bsrr.write(|w| w.br13().set_bit());
+    delay_ms(250);
+}
+
+fn delay_ms(ms: usize) {
+    tick_delay(500 * ms);
+}
+
+fn tick_delay(ticks: usize) {
+    // Think we can consider the execution of a nop instruction safe.
+    (0..ticks).for_each(|_| unsafe { asm!("nop") });
+}
